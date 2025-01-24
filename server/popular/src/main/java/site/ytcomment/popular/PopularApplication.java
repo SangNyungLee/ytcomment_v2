6409daa4d331df6a32bb8caecffbc84245aac499
SangNyungLee <lsy6747@gmail.com>
2025-01-24 20:19:13 +0900

    Directory 변경

---
+++ server/popular/src/main/java/site/ytcomment/popular/PopularApplication.java
@@ -0,0 +1,13 @@
+package site.ytcomment.popular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopularApplication.class, args);
+	}
+
+}
---
+++ server/popular/src/test/java/site/ytcomment/popular/PopularApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ytcomment.popular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
