6b683fe9d520f869f113343fe4846a5c5f56bfb9
SangNyungLee <lsy6747@gmail.com>
2025-03-04 15:22:54 +0900

    SecurityConfig 수정

--- server/popular/src/main/java/site/ytcomment/popular/config/SecurityConfig.java
+++ server/popular/src/main/java/site/ytcomment/popular/config/SecurityConfig.java
@@ -1,46 +1,23 @@
 package site.ytcomment.popular.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import site.ytcomment.popular.security.JwtFilter;
-import site.ytcomment.popular.security.JwtTokenProvider;
 
 @Configuration
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfig corsConfig) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
-
-                // JWT는 세션을 사용하지 않으므로 STATELESS로 설정
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // 인증/인가 예외처리 핸들러 설정
-//                .exceptionHandling(exception -> exception
-//                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                        .accessDeniedHandler(jwtA))
-
                 .authorizeHttpRequests(auth -> auth
-                        // 인증없이 가능한 경로
                         .requestMatchers("/api/**", "/**").permitAll()
-                        // 정적 리소스는 허용
-                        .requestMatchers("/", "/favicon.ico", "/css/**").permitAll()
-                        // 그 외 모든 요청에는 인증 필요
-                        .anyRequest().authenticated()) // 인증 필요
-                .addFilterBefore(new JwtFilter())
+                        .anyRequest().authenticated() // 인증 필요
                 );
         return http.build();
     }
@@ -49,13 +26,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         // 비밀번호를 해싱할 때 내부적으로 salt를 자동으로 생성하여 보안성을 높여줌
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-
     }
 
 }
