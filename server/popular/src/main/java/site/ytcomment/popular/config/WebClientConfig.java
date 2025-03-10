e16743e912158fa350dc819330b736ef2c5fed45
SangNyungLee <lsy6747@gmail.com>
2025-03-10 19:38:34 +0900

    Modify WebClient Cors

--- server/popular/src/main/java/site/ytcomment/popular/config/WebClientConfig.java
+++ server/popular/src/main/java/site/ytcomment/popular/config/WebClientConfig.java
@@ -3,7 +3,6 @@ package site.ytcomment.popular.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,15 +15,4 @@ public class WebClientConfig implements WebMvcConfigurer {
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
-
-    // CORS 설정 -> spring Security에서 설정해서 지워줘도 됨
-    // 만약에 spring security없으면 이거 설정해줘야되고
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:5173")
-//                .allowedHeaders("*")
-//                .allowCredentials(true)
-//                .maxAge(3600);
-//    }
 }
