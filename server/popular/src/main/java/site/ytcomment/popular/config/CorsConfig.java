4d801edf928c874b8251dc01cd8bc14849ef44f6
SangNyungLee <lsy6747@gmail.com>
2025-03-18 19:22:38 +0900

    배포서버 cors 설정 추가

--- server/popular/src/main/java/site/ytcomment/popular/config/CorsConfig.java
+++ server/popular/src/main/java/site/ytcomment/popular/config/CorsConfig.java
@@ -1,20 +1,28 @@
 package site.ytcomment.popular.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // 프론트엔드 URL 허용
+        // 여러 개의 도메인을 올바르게 리스트로 변환
+        List<String> allowedOriginList = Arrays.asList(allowedOrigins.split(","));
+
+        configuration.setAllowedOrigins(allowedOriginList); // 허용할 도메인 목록
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
         configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 쿠키 및 인증정보 허용
