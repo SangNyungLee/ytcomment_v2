7c086c0b4c3b766f4b2079f56d656a94239a6e15
SangNyungLee <lsy6747@gmail.com>
2025-03-19 14:47:38 +0900

    userName오류 수정

--- server/popular/src/main/java/site/ytcomment/popular/Service/DTO/EmailSignUpServiceDTO.java
+++ server/popular/src/main/java/site/ytcomment/popular/Service/DTO/EmailSignUpServiceDTO.java
@@ -24,7 +24,7 @@ public class EmailSignUpServiceDTO {
                     .userId(this.userId)
                     .userEmail(this.userEmail)
                     .userPw(pw)
-                    .userName(this.userPw)
+                    .userName(this.userName)
                     .social(SocialEnum.이메일.getCode())
                     .build();
         }
