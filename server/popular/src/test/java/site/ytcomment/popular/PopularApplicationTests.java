8632b3ef16d817cf178321f3fb5dc7a1d92556b0
SangNyungLee <lsy6747@gmail.com>
2025-04-04 02:28:51 +0900

    환경변수 dev, prod 분리

--- server/popular/src/test/java/site/ytcomment/popular/PopularApplicationTests.java
+++ server/popular/src/test/java/site/ytcomment/popular/PopularApplicationTests.java
@@ -2,8 +2,10 @@ package site.ytcomment.popular;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class PopularApplicationTests {
 
 	@Test
